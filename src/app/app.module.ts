81e6be5948c86c2efb801df1c25db29f03d31ab8
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2021-04-22 17:38:56 +0900

    show product list with dummy images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
+import { ProductListComponent } from './common/product-list/product-list.component';
+import { ProductItemComponent } from './common/product-item/product-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProductListComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
